3e7457e9687344422622b7502872c102e36f396e
hemawrith <ueUR2vTaLe1*>
2021-06-04 16:50:56 +0300

    Committing stuff

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Main {
+
+
+    public static void findFirst(int from, int to) {
+        List<Integer> nums = IntStream.rangeClosed(from, to).boxed().collect(Collectors.toList());
+        int y = 3;
+        nums.stream().filter(x -> x % y == 0).findFirst().ifPresent(System.out::println);
+    }
+
+    public static void mapTry() {
+        Stream.of(1, 2, 3).map(x -> x * x).forEach(System.out::println);
+    }
+
+    public static void main(String[] args) {
+
+        findFirst(1, 1000);
+        mapTry();
+    }
+}
